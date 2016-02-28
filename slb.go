2011c80c69cc1c1cfcb99db015ecc8ce14157d89
lysu <sulifx@gmail.com>
2016-02-28 23:16:20 +0800

    docs(api): Improve api and add comment

---
+++ slb.go
@@ -0,0 +1,6 @@
+// lb presents simple client-based LoadBalancer
+// provide:
+// - distribute request to nodes in node-list
+// - circuitBreaker break request when successive error or high concurrent number
+// - retry in one server or other server
+package lb
